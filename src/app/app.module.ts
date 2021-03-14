9a388eedf7a8f590978dd1a09d477d9acc41c85b
Sagar Agrawal <>
2021-03-14 12:23:17 +0530

    updating resume component text, styles, icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faHome, faAddressCard, faGraduationCap,
-  faBook, faSuitcase, faNewspaper, faMapMarkedAlt, faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+  faBook, faSuitcase, faNewspaper, faMapMarkedAlt, faPhoneAlt, faEnvelope, faAsterisk } from '@fortawesome/free-solid-svg-icons';
 //import { faSquare, faCheckSquare, faHome } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faUser, faBuilding } from '@fortawesome/free-regular-svg-icons';
 import { faStackOverflow, faGithub, faMedium, 
   faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
@@ -41,6 +41,6 @@ export class AppModule {
   constructor(private library: FaIconLibrary) {
     
     library.addIcons(faUser, faGraduationCap, faAddressCard, faBook, faSuitcase, faNewspaper, faMapMarkedAlt,
-      faPhoneAlt, faEnvelope, faHome, faStackOverflow, faGithub, faLinkedinIn, faTwitter, faMedium);
+      faPhoneAlt, faEnvelope, faHome, faStackOverflow, faGithub, faLinkedinIn, faTwitter, faMedium, faAsterisk, faBuilding);
   }
 }
